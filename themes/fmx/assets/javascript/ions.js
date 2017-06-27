1b42e0ba5627db32e3c5c53aacc881840cc5a1fd
Alek Mieczkowski <alekmieczkowski@gmail.com>
2017-06-26 21:47:36 -0400

    first commit

---
+++ themes/fmx/assets/javascript/ions.js
@@ -0,0 +1,12 @@
+  window.onload=function(){ 
+
+  	var ion = new Ion('particles', {
+        shape: 'circle',
+        density: 20,
+        color: '#FFDF00'
+       },
+       { canvasBackground: 'rgba(0,0,0,1)' }
+    ); 
+
+   };
+
